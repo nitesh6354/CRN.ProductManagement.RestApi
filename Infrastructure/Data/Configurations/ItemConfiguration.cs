b172200a45ed55b025f2449aab9c0680775a96d9
Nitesh <patilnitesh1312@gmail.com>
2026-06-17 15:08:24 +0530

    Added ApplicationDbContext and entity configurations

---
+++ Infrastructure/Data/Configurations/ItemConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.Configurations;
+
+public class ItemConfiguration : IEntityTypeConfiguration<Item>
+{
+    public void Configure(EntityTypeBuilder<Item> builder)
+    {
+        builder.ToTable("Item");
+
+        builder.HasKey(x => x.Id);
+
+        builder.HasOne(x => x.Product)
+               .WithMany(x => x.Items)
+               .HasForeignKey(x => x.ProductId);
+
+        builder.Property(x => x.Quantity)
+               .IsRequired();
+    }
+}
---
+++ Infrastructure/Data/Configurations/ProductConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.Configurations;
+
+public class ProductConfiguration : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.ToTable("Product");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.ProductName)
+               .IsRequired()
+               .HasMaxLength(255);
+
+        builder.Property(x => x.CreatedBy)
+               .IsRequired()
+               .HasMaxLength(100);
+    }
+}
